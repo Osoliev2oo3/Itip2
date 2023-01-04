098e41449c495244359a092b38b51a37ae0d73db
Osoliev2oo3 <solievolimhodza@gmail.com>
2023-01-04 17:04:32 +0300

    second lab

---
+++ src/Lab2.java
@@ -0,0 +1,67 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Console app for calculating the square of triangle of given points
+ * */
+public class Lab2 {
+    public static void main(String[] args) {
+        Point3d point1 = new Point3d();
+        Point3d point2 = new Point3d();
+        Point3d point3 = new Point3d();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input x coord for 1 point:");
+        point1.setX(scanner.nextDouble());
+        System.out.print("Input y coord for 1 point:");
+        point1.setY(scanner.nextDouble());
+        System.out.print("Input z coord for 1 point:");
+        point1.setZ(scanner.nextDouble());
+        System.out.print("Input x coord for 2 point:");
+        point2.setX(scanner.nextDouble());
+        System.out.print("Input y coord for 2 point:");
+        point2.setY(scanner.nextDouble());
+        System.out.print("Input z coord for 2 point:");
+        point2.setZ(scanner.nextDouble());
+        System.out.print("Input x coord for 3 point:");
+        point3.setX(scanner.nextDouble());
+        System.out.print("Input y coord for 3 point:");
+        point3.setY(scanner.nextDouble());
+        System.out.print("Input z coord for 3 point:");
+        point3.setZ(scanner.nextDouble());
+
+
+        scanner.close();
+        if (point1.equals(point2) || point2.equals(point3) || point1.equals(point3)) {
+            System.out.println("some points are equal - the square = 0");
+        } else {
+            double area = computeArea(point1, point2, point3);
+            System.out.println("the square of the triangle of 3 dots: " + area);
+        }
+
+    }
+
+    /**
+     * @return the square of the triangle of given points
+     * @throws IllegalStateException if any of the parameters is null
+     * */
+    public static double computeArea(Point3d point1, Point3d point2, Point3d point3) {
+        if (point1 == null || point2 == null || point3 == null) {
+            throw new IllegalStateException(
+                    "all of the given params must be null. " +
+                            "Got point1: " + point1 + ", point2: " + point2 + ", point3: " + point3
+            );
+        }
+
+        double firstSide = point1.distanceTo(point2);
+        double secondSide = point2.distanceTo(point3);
+        double thirdSide = point1.distanceTo(point3);
+        double p = (firstSide + secondSide + thirdSide) / 2.0;
+        double result =  Math.sqrt(
+                p
+                        * (p - firstSide)
+                        * (p - secondSide)
+                        * (p - thirdSide)
+        );
+        return Double.parseDouble(String.format(Locale.ROOT, "%.2f", result));
+    }
+}
---
+++ src/Point3d.java
@@ -0,0 +1,69 @@
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * 3d point class.
+ * Warning: the class is mutable
+ * Two Pont3d objects are equals if their getX(), getY(), zCoord fields are equal.
+ */
+public class Point3d extends Point2d {
+    /**
+     * zCoord
+     * */
+    private double zCoord;
+
+    public Point3d(double xCoord, double yCoord, double zCoord) {
+        super(xCoord, yCoord);
+        this.zCoord = zCoord;
+    }
+
+    /**
+     * Default value for all fields is 0.0
+     */
+    public Point3d() {
+        this(0, 0, 0);
+    }
+
+    public void setZ(double zCoord) {
+        this.zCoord = zCoord;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point3d)) return false;
+        Point3d point3d = (Point3d) o;
+        return point3d.getX() == getX()
+                && point3d.getY() == getY()
+                && point3d.zCoord == zCoord;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY(), zCoord);
+    }
+
+    /**
+     * returns the distance between two points rounded to 2 decimal places
+     *
+     * @throws IllegalStateException if the point parameter is null
+     */
+    public double distanceTo(Point3d point) {
+        if (point == null) throw new IllegalStateException("point parameter must be not null");
+        double distance = Math.sqrt(
+                Math.pow(point.getX() - getX(), 2)
+                        + Math.pow(point.getY() - getY(), 2)
+                        + Math.pow(point.zCoord - zCoord, 2)
+        );
+        return Double.parseDouble(String.format(Locale.ROOT, "%.2f", distance));
+    }
+
+    @Override
+    public String toString() {
+        return "Point3d{" +
+                "getX()=" + getX() +
+                ", getY()=" + getY() +
+                ", zCoord=" + zCoord +
+                '}';
+    }
+}
